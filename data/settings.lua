6b4b57f884811bb99fa0cfcd96ec107e7b9e3a8b
KateFFXI <52470877+KateFFXI@users.noreply.github.com>
2021-07-09 17:44:51 -0400

    New location

--- data/settings.lua
+++ data/settings.lua
@@ -7,8 +7,8 @@ return {
         ['min'] = {
             ['curaga'] = 1,
             ['cure'] = 3,
-            ['waltz'] = 2,
-            ['waltzga'] = 1
+            ['waltz'] = 3,
+            ['waltzga'] = 2
         }
     },
     ['ignoreTrusts'] = false,
